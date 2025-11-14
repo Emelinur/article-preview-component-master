8c53d7d7600f6be2b9e52c9fe997041adcf3e16f
Emelinur <emelinuryzgc@gmail.com>
2025-11-14 17:55:46 +0300

    update main.js and style.css

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
-   let btn_share=document.querySelector("#btn_share");
-   
-   btn_share.addEventListener("click", ()=>{
-    
-   })
+let btn_share = document.querySelector("#btn_share");
+let share_popover = document.querySelector(".share-popover");
+
+btn_share.addEventListener("click", () => {
+  if (share_popover.style.display === "flex") {
+    share_popover.style.display = "none";
+  } else {
+    share_popover.style.display = "flex";
+  }
+});
