99c27224f9d9284ddc3caa1b99778ef3c8d4b520
Emelinur <emelinuryzgc@gmail.com>
2025-11-20 12:35:46 +0300

    update  index.html,style.css and main.js

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
-const btnShare = document.querySelector('#btn_share');
-const card = document.querySelector('.card');
-const share_popover=document.querySelector(".share-popover")
+const shareBtn = document.getElementById("btn_share");
+const card = document.querySelector(".card");
 
-btnShare.addEventListener('click', () => {
-  card.classList.toggle('card--share-open');
-share_popover.classList.add('share-btn--popup')
+shareBtn.addEventListener("click", () => {
+  card.classList.toggle("card--share-open");
 });
